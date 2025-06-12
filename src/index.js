1ba13eed65ca059ce0cf4b835c9767e518c78899
maxime <maximelarquetoux@gmail.com>
2025-06-12 16:08:19 +0200

    Sequelize configuration and initialization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import express from 'express';
+import sequelize from '../config/db.js';
 
 const app= express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+const start= async () =>{
+    try{
+        await sequelize.authenticate();
+        console.log('Database connection has been established successfully');
+
+        app.listen(port, ()=>{
+            console.log(`The app listening on port ${port}`);
+        });
+    } catch(error){
+        console.log('Failed to start the application: ', error);
+        process.exit(1);
+    }
+};
+
+app.get('/', (req, res)=>{
+    res.send('Hello World !');
 });
 
 
-app.listen(3000, () =>
-    console.log(`The app listening on port ${port}`),
-);
+start();
